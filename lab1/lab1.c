5264d63dd107032493eb1057b4ba03d0d42b69cc
linni <linni.cai.lc@gmail.com>
2021-09-06 16:50:14 -0400

    finiish lab1

---
+++ lab1/lab1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// name: Brent Hailpern
+// email: b.hailpern@northeastern.edu
+
+int main() {
+  printf("Hello World.  This is Brent.\n");
+  printf("I am assignment 1 for Fall 2021.\n");
+  return 0;
+}
+
